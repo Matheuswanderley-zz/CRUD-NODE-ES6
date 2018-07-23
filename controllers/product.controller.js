2396a0606cfda0ff0cce505955272457f70b1cd7
Matheuswanderley <matheuswanderley1988@gmail.com>
2018-07-23 20:24:38 -0300

    done

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product.model');
 
-exports.test = function(req, res){
-    res.send('Greetings from test controller!!')
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
 };
 
 exports.product_create = function (req, res) {
-    let product = new Product(
+    var product = new Product(
         {
             name: req.body.name,
             price: req.body.price
@@ -17,5 +17,26 @@ exports.product_create = function (req, res) {
             return next(err);
         }
         res.send('Product Created successfully')
+    })
+};
+
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
     })
 };
